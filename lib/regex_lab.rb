54d0df74414fe68d53c9f216e8c85da2602ecaac
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-17 07:38:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,5 +22,9 @@ end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+      return false
+    else
+      return true
+    end
 end
